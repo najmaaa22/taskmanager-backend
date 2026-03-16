89db8829b687ac8ae8c13487dab9cb04adfd94e8
najma manaf <najmamanaf18@gmail.com>
2026-03-16 10:48:30 +0530

    frontend url added

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://najmataskmanager.vercel.app/",
+//   credentials: true
+}));
 app.use(express.json())
 
 
